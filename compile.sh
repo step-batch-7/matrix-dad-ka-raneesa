ac9cfc08c294286bbe55ca5f48bfaf7519a85773
KarriBhuvanaDeepika <bhuvanadeepika017@gmail.coma>
2020-10-22 16:18:26 +0530

    modified compile.sh and run.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,7 @@
+set -eu;
+
+echo "Cleaning...\n"
 rm -rf out/*
+
+echo "Compiling...\n"
 javac -d out -cp lib/hamcrest-core-1.3.jar:lib/junit-4.13.1.jar $(find . -name "*.java" )
--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-java -cp out:lib/hamcrest-core-1.3.jar:lib/junit-4.13.1.jar org.junit.runner.JUnitCore com.step.maths.MatrixTest
+set -eu;
+
+sh compile.sh
+
+echo "Running tests...\n"
+java -cp out:lib/hamcrest-core-1.3.jar:lib/junit-4.13.1.jar org.junit.runner.JUnitCore $1
