78328ac18232c2d6397d8b607b086246a56fa88b
KarriBhuvanaDeepika <bhuvanadeepika017@gmail.coma>
2020-10-22 15:17:15 +0530

    added tests for equals method in matrix

--- test/com/step/maths/MatrixTest.java
+++ test/com/step/maths/MatrixTest.java
@@ -1,6 +1,8 @@
 package com.step.maths;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 public class MatrixTest{
@@ -10,4 +12,37 @@ public class MatrixTest{
     Matrix matrix = new Matrix(data, 2, 2);
     assertEquals(matrix.toString(), "Matrix [[1, 2], [4, 5]]");
   }
+
+  @Test
+  public void shouldGiveTrueForMatricesOfSameReference(){
+    int[][] data = {{1,2}, {4,5}};
+    Matrix matrix = new Matrix(data, 2, 2);
+    assertTrue(matrix.equals(matrix));
+  }
+
+  @Test
+  public void shouldGiveTrueForSameMatrices(){
+    int[][] data1 = {{1,2}, {4,5}};
+    Matrix matrix1 = new Matrix(data1, 2, 2);
+    int[][] data2 = {{1,2}, {4,5}};
+    Matrix matrix2 = new Matrix(data2, 2, 2);
+    assertTrue(matrix1.equals(matrix2));
+  }
+
+  @Test
+  public void shouldGiveFalseForDifferentMatrices(){
+    int[][] data1 = {{1,2}, {4,5}};
+    Matrix matrix1 = new Matrix(data1, 2, 2);
+    int[][] data2 = {{1,2}};
+    Matrix matrix2 = new Matrix(data2, 1, 2);
+    assertFalse(matrix1.equals(matrix2));
+  }
+
+  @Test
+  public void shouldGiveFalseForDifferenceInstanceObjects(){
+    int[][] data = {{1,2}, {4,5}};
+    Matrix matrix = new Matrix(data, 2, 2);
+    assertFalse(matrix.equals(new Object()));
+  }
+
 }
