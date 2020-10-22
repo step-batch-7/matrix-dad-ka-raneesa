e0b80bba5df6b3d359e1d65b68527a2e6e7a242a
KarriBhuvanaDeepika <bhuvanadeepika017@gmail.coma>
2020-10-22 16:43:37 +0530

    added initMatrices method and used org.junit.Test package in MatrixTest.java

--- test/com/step/maths/MatrixTest.java
+++ test/com/step/maths/MatrixTest.java
@@ -3,79 +3,67 @@ package com.step.maths;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MatrixTest{
+  private Matrix matrix1;
+  private Matrix matrix2;
+
+  @Before
+  public void initMatrices() {
+    int[][] arr1 = {{1,2,3},{2,3,4},{3,4,5}};
+    int[][] arr2 = {{1,3,4},{1,5,4},{2,4,3}};
+    this.matrix1 = new Matrix(arr1, 3, 3);
+    this.matrix2 = new Matrix(arr2, 3, 3);
+  }
+
   @Test
   public void shouldGiveStringRepresentationOfMatrix(){
-    int[][] data = {{1,2}, {4,5}};
-    Matrix matrix = new Matrix(data, 2, 2);
-    assertEquals(matrix.toString(), "Matrix [[1, 2], [4, 5]]");
+    assertEquals(this.matrix1.toString(), "Matrix [[1, 2, 3], [2, 3, 4], [3, 4, 5]]");
   }
 
   @Test
   public void shouldGiveTrueForMatricesOfSameReference(){
-    int[][] data = {{1,2}, {4,5}};
-    Matrix matrix = new Matrix(data, 2, 2);
-    assertTrue(matrix.equals(matrix));
+    assertTrue(this.matrix1.equals(this.matrix1));
   }
 
   @Test
   public void shouldGiveTrueForSameMatrices(){
-    int[][] data1 = {{1,2}, {4,5}};
-    int[][] data2 = {{1,2}, {4,5}};
-    Matrix matrix1 = new Matrix(data1, 2, 2);
-    Matrix matrix2 = new Matrix(data2, 2, 2);
-    assertTrue(matrix1.equals(matrix2));
+    int[][] data = {{1,2,3},{2,3,4},{3,4,5}};
+    Matrix expected = new Matrix(data, 3, 3);
+    assertTrue(this.matrix1.equals(expected));
   }
 
   @Test
   public void shouldGiveFalseForDifferentMatrices(){
-    int[][] data1 = {{1,2}, {4,5}};
-    int[][] data2 = {{1,2}};
-    Matrix matrix1 = new Matrix(data1, 2, 2);
-    Matrix matrix2 = new Matrix(data2, 1, 2);
-    assertFalse(matrix1.equals(matrix2));
+    assertFalse(this.matrix1.equals(this.matrix2));
   }
 
   @Test
   public void shouldGiveFalseForDifferenceInstanceObjects(){
-    int[][] data = {{1,2}, {4,5}};
-    Matrix matrix = new Matrix(data, 2, 2);
-    assertFalse(matrix.equals(new Object()));
+    assertFalse(this.matrix1.equals(new Object()));
   }
 
   @Test
   public void shouldAddEqualRowAndColumnLengthOfMatrices(){
-    int[][] arr1 = {{1,2,3},{2,3,4},{3,4,5}};
-    int[][] arr2 ={{1,3,4},{1,5,4},{2,4,3}};
     int[][] data = {{2,5,7},{3,8,8},{5,8,8}};
-    Matrix matrix1 = new Matrix(arr1, 3, 3);
-    Matrix matrix2 = new Matrix(arr2, 3, 3);
     Matrix expected = new Matrix(data,3, 3);
-    assertEquals(matrix1.add(matrix2), expected);
+    assertEquals(this.matrix1.add(this.matrix2), expected);
   }
 
   @Test
   public void shouldSutractEqualRowAndColumnLengthOfMatrices(){
-    int[][] arr1 = {{1,2,3},{2,3,4},{3,4,5}};
-    int[][] arr2 ={{1,3,4},{1,5,4},{2,4,3}};
     int[][] data = {{0,-1,-1},{1,-2,0},{1,0,2}};
-    Matrix matrix1 = new Matrix(arr1, 3, 3);
-    Matrix matrix2 = new Matrix(arr2, 3, 3);
     Matrix expected = new Matrix(data,3, 3);
-    assertEquals(matrix1.subtract(matrix2), expected);
+    assertEquals(this.matrix1.subtract(this.matrix2), expected);
   }
 
   @Test
   public void shouldMultiplyEqualRowAndColumnLengthOfMatrices(){
-    int[][] arr1 = {{1,2,3},{2,3,4},{3,4,5}};
-    int[][] arr2 ={{1,3,4},{1,5,4},{2,4,3}};
     int[][] data = {{9,25,21},{13,37,32},{17,49,43}};
-    Matrix matrix1 = new Matrix(arr1, 3, 3);
-    Matrix matrix2 = new Matrix(arr2, 3, 3);
     Matrix expected = new Matrix(data,3, 3);
-    assertEquals(matrix1.multiply(matrix2), expected);
+    assertEquals(this.matrix1.multiply(this.matrix2), expected);
   }
 
   @Test
@@ -94,8 +82,6 @@ public class MatrixTest{
 
   @Test
   public void shouldGiveDetOfMatrixOfRowAndColumnOfLengthMoreThanTwo(){
-    int[][] arr = {{2,4,7},{5,2,3},{2,4,6}};
-    Matrix matrix = new Matrix(arr, 3, 3);
-    assertEquals(matrix.determinant(), 16);
+    assertEquals(this.matrix2.determinant(), -10);
   }
 }
