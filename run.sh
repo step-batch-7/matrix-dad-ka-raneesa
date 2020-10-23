980f1c3ff09e12bd70ab8f7493533fc4bf13b99d
KarriBhuvanaDeepika <bhuvanadeepika017@gmail.coma>
2020-10-23 18:14:51 +0530

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
+# /bin/sh
+
 set -eu;
 
-sh compile.sh
+./compile.sh
+
+TESTNAMES=`find test -name "*Test.java" | sed "s/test\///" | sed "s/.java//" | tr "/" "."`
 
 echo "Running tests...\n"
-java -cp out:lib/hamcrest-core-1.3.jar:lib/junit-4.13.1.jar org.junit.runner.JUnitCore $1
+java -cp out:lib/hamcrest-core-1.3.jar:lib/junit-4.13.1.jar org.junit.runner.JUnitCore $TESTNAMES
